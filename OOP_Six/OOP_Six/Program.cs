dbb0916a430af05e6b46600a017206a2f65ab07f
ImpersonaI <43351355+ImpersonaI@users.noreply.github.com>
2020-11-18 16:46:23 +0300

    Add files via upload

---
+++ OOP_Six/OOP_Six/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP_Six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Property<int, double, int> property = new Property<int, double, int>("Property", 4, 4, 4, 4, 3.5);
+            Property<double, double, double> property1 = new Property<double, double, double>("Property 1", 4.3, 4.3, 4.2, 4.1, 3.2);
+            Property<int, int, int> property2 = new Property<int, int, int>("Property 2", 4, 4, 4, 4, 3);
+            Property<double, int, double> property3 = new Property<double, int, double>("Property 3", 4.3, 4.3, 4.3, 4.3, 3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP_Six/OOP_Six/Property.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Six
+{
+    class Property<T, T1, T2>
+    {
+        public string Address { get; set; }
+        public T2 Latitude { get; set; }
+        public T2 Longtitude { get; set; }
+        public T Length { get; set; }
+        public T Width { get; set; }
+        public T1 NumberOfFlats { get; set; }
+
+        public Property(string address, T2 latitude, T2 longtitude, T length, T width, T1 numberofflats)
+        {
+
+            Address = address;
+            Latitude = latitude;
+            Longtitude = longtitude;
+            Length = length;
+            Width = width;
+            NumberOfFlats = numberofflats;
+            GetInfo();
+        }
+
+        public void GetInfo ()
+        {
+
+            Console.WriteLine("Address: " + Address);
+            Console.WriteLine("Широта: " + Latitude);
+            Console.WriteLine("Долгота: " + Longtitude);
+            Console.WriteLine("Длина: " + Length);
+            Console.WriteLine("Ширина: " + Width);
+            Console.WriteLine("Число этажей : " + NumberOfFlats);
+            Console.WriteLine();
+        }
+    }
+}
